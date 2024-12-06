08476d95d25657a1e77d87fcd3d6870e31a27c8c
Perazzolo Sean <sean.perazzolo@gmail.com>
2024-12-06 17:27:35 +0100

    D06

---
+++ 2024/src/jour06/jour06.kt
@@ -0,0 +1,102 @@
+package jour06
+
+import helper.Helper
+
+fun main() {
+    Helper.startTimer()
+    val input = Helper.readInput("06")
+    Part1And2.main(input)
+    Helper.stopTimer()
+}
+object Part1And2 {
+    // Constants
+    const val OBSTACLE = '#'
+    const val GUARD = '^'
+    const val EMPTY = '.'
+
+
+    // Enums
+    enum class Direction(val dx: Int, val dy: Int) {
+        UP(-1, 0),
+        DOWN(1, 0),
+        LEFT(0, -1),
+        RIGHT(0, 1);
+
+        fun turnRight() : Direction {
+            return when (this) {
+                UP -> RIGHT
+                RIGHT -> DOWN
+                DOWN -> LEFT
+                LEFT -> UP
+            }
+        }
+    }
+
+    // Data classes
+    data class Position(val pos: Pair<Int,Int>, val dir: Direction)
+
+    // Functions
+    fun makeGrid(input:String): List<List<Char>> {
+        return input.lines().map { it.toList() }.filterNot { it.isEmpty() }
+    }
+    fun findGuard(grid: List<List<Char>>): Pair<Int,Int> {
+        val x = grid.indexOfFirst { it.contains(GUARD) }
+        val y = grid[x].indexOf(GUARD)
+        return x to y
+    }
+    private fun move(pos: Position, grid: List<List<Char>>) : Position {
+        val newPos = pos.pos.first + pos.dir.dx to pos.pos.second + pos.dir.dy
+
+            if (grid[newPos.first][newPos.second] == OBSTACLE)
+                return Position(pos.pos, pos.dir.turnRight())
+            else
+                return Position(newPos, pos.dir)
+    }
+    fun detectLoop(grid: List<List<Char>>, startPos: Position) : Boolean {
+        val visited = mutableSetOf<Position>()
+        var pos = startPos
+        try {
+            while (true) {
+                pos = move(pos, grid)
+                // Looping detected if we are on a cell we have already visited with the same direction
+                if(!visited.add(pos)) {
+                    return true
+                }
+            }
+        } catch (e : Exception) {
+            return false
+        }
+    }
+
+    fun main(input: String) {
+        val defaultGrid = makeGrid(input)
+        val startPos = Position(findGuard(defaultGrid), Direction.UP)
+        val visited : MutableSet<Position> = mutableSetOf(startPos)
+
+        // Part 1
+        try {
+            var pos = startPos
+            while (true) {
+                pos = move(pos, defaultGrid)
+                visited.add(pos)
+            }
+        } catch (_: Exception) { }
+        // only keep the cells and not the direction, remove all duplicates cells
+        val visitedFiltered = visited.map{it.pos}.toSet()
+
+        //  Part 2
+        var loopCounter = 0
+        val grid = defaultGrid.map { it.toMutableList() }.toMutableList()
+        visitedFiltered.forEach {  (x,y) ->
+                if(defaultGrid[x][y] == EMPTY) {
+                    grid[x][y] = OBSTACLE
+                    loopCounter += if(detectLoop(grid, startPos)) 1 else 0
+                    grid[x][y] = EMPTY
+                }
+        }
+        println("Part 1 : ${visitedFiltered.size}")
+        println("Part 2 : Found $loopCounter loops")
+    }
+}
+
+
---
+++ 2024/src/template/jourXX.kt
@@ -0,0 +1,21 @@
+package template
+
+import helper.Helper
+
+
+fun main() {
+    val input = Helper.readInput("XX")
+
+}
+
+object Part1 {
+    fun main(input: String) {
+
+    }
+}
+
+object Part2 {
+    fun main(input: String) {
+
+    }
+}
